b66d95fa7bb3e253bc4deb43c237091bca72abbc
ElOshino <santinso@hotmail.com>
2020-11-18 14:30:31 -0500

    master

--- src/database.js
+++ src/database.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
 const {BIBLIOTECA_UC_MONGODB_HOST, BIBLIOTECA_UC_MONGODB_DATABASE } = process.env;
-const MONGODB_URI = `mongodb://${BIBLIOTECA_UC_MONGODB_HOST}/${BIBLIOTECA_UC_MONGODB_DATABASE}`
+//const MONGODB_URI = `mongodb://${BIBLIOTECA_UC_MONGODB_HOST}/${BIBLIOTECA_UC_MONGODB_DATABASE}`
+const MONGODB_URI = `mongodb+srv://Admin:TXIFNvpUFTg8s9F5@cluster0.u2yo7.mongodb.net/biblioteca?retryWrites=true&w=majority`;
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true
 }).then(db => console.log('Database is conected'))
-.catch(err => console.log(MONGODB_URI));
+.catch(err =>  console.error(`Error connecting to the database. \n${err}`));
+
+
+
+
